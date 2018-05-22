e22d6889bddc0c3e2b15f229a7d87179b3ed269c
Martina Schwerdtfeger <codekrams@gmail.com>
2018-05-22 20:05:04 +0200

    Add c and readme file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # c-bit-display
+Erstellen Sie folgendes Programm: ein Benutzer gibt eine vorzeichenlose Zahl ein und
+das Programm gibt aus, an welchen Stelle die Bits gesetzt sind.
+z.B. Zahl: 3
+Bit gesetzt: 1. Stelle
+Bit gesetzt: 2. Stelle
---
+++ c-bit-disyplay.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int eingabe;
+    int bitwert = 128;
+    int bitzaehler = 8;
+    int i = 0;
+
+    printf("Bitte geben Sie eine Zahl zwischen 1 und 255 ein\n");
+    scanf("%i", &eingabe);
+
+    while (eingabe>0){
+        if(eingabe/bitwert==1){
+            printf("Bit gesetzt: %i. Stelle\n", bitzaehler);
+        }
+        eingabe=eingabe%bitwert;
+        bitwert = bitwert/2;
+        bitzaehler = bitzaehler-1;
+    }
+}
